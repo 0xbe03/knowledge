55e9c6ee8355272c4f86322b1614240ee7b0a242
Reza Aghajani <rezaaghajani@protonmail.com>
2020-10-16 13:18:25 +0330

    Learning about require and modules in NodeJs ✔

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,1 +1,4 @@
-console.log("Hello World!")
+var counter = require('./count')
+
+console.log(counter(["test","test2","test3"]))
+
---
+++ Nodejs/count.js
@@ -0,0 +1,5 @@
+function counter(arr) {
+    return "The number of items is " + arr.length
+}
+
+module.exports = counter
