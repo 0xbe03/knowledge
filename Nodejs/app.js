5c8013d19ce8c07984bb68f15cdd74d2b7f65a8b
Reza Aghajani <rezaaghajani@protonmail.com>
2020-10-17 07:28:45 +0330

    Learn about view engines in NodeJs and EJS ✔

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -2,20 +2,25 @@ const express = require('express')
 
 const app = express()
 
+// register view engine
+app.set('view engine' , 'ejs')
+
+
 app.listen(3000)
 
 app.get('/' , (req,res) => {
-    res.sendFile('./view/index.html' , {root: __dirname})
+    res.render('index')
 })
 
 app.get('/about' , (req,res) => {
-    res.sendFile('./view/about.html' , {root: __dirname})
+    res.render('about')
 })
 
-app.get('/about-us' , (req,res) => {
-    res.redirect('/about')
+app.get('/blogs/create' , (req,res) => {
+    res.render('create')
 })
+
 
 app.use((req,res) => {
-    res.status(404).sendFile('./view/404.html' , {root: __dirname})
+    res.status(404).render('404')
 })
