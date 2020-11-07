1234783933433256f8be2fb88bf110191f592169
Reza Aghajani <rezaaghajani@protonmail.com>
2020-11-07 19:14:10 +0330

    Learning about for loops in GoLang ✔

--- golang/app.go
+++ golang/app.go
@@ -1,19 +1,10 @@
-package main
+package main 
 
 import "fmt"
 
-func add(x float64,y float64) float64 {
-	return x+y
-}
-
-func multiple(a,b string) (string,string) {
-	return a,b
-}
-
 func main() {
-	w1,w2 := "Hello","Reza"
-
-	fmt.Println(multiple(w1,w2))
-
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
+	}
 
 }
