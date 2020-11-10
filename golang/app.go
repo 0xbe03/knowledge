c68d68e8b46f66ecaf90ddf02eeac266153a18c5
Reza Aghajani <rezaaghajani@protonmail.com>
2020-11-10 19:26:29 +0330

    Learn about methods in GoLang ✔

--- golang/app.go
+++ golang/app.go
@@ -1,13 +1,24 @@
 package main
 
-import ("fmt"
-		"net/http")
+import "fmt"
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
+const usinteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.609
+
+type car struct {
+	gas_pedal uint16
+	brake_pedal uint16
+	string_wheel int16
+	top_speed_kmh float64
+}
+
+func (c car) kmh() float64 {
+	return c.top_speed_kmh * kmh_multiple
 }
 
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.ListenAndServe(":8000" , nil)
+	a := car{gas_pedal: 22300, brake_pedal: 200, string_wheel: 4, top_speed_kmh:360.0}
+	fmt.Println(a.gas_pedal)
+	fmt.Println(a.kmh())
+
 }
