a561e09a4b18d80244df139c339918a19cac8310
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-01 11:51:02 +0330

    Working With Multiple Components

--- react/tut/src/App.js
+++ react/tut/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   const title = "Hi"
   return (
     <div className="App">
       <Navbar />
-      <h1>App Component</h1>
-      <h2>{title}</h2>
+      <Home />
+      <h2> {title} </h2>
     </div>
   );
 }
