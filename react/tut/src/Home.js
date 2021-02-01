48c860b2947885be30614d495b2f4e6990f20d27
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-01 12:05:06 +0330

    Working with Click Events

--- react/tut/src/Home.js
+++ react/tut/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello')
+    }
+
+    const handleclickAgain = (name) => {
+        console.log('hello ' + name)
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {handleclickAgain('reza')}}>Click Me Again</button>
         </div>
      );
 }
