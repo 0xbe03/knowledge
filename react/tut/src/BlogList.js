9d8079439e7cd58f95a74d98d1853c311540ec5e
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-01 12:30:39 +0330

    Learn How to use Props in React

---
+++ react/tut/src/BlogList.js
@@ -0,0 +1,15 @@
+const BlogList = (props) => {
+    const blogs = props.blogs;
+    return ( 
+        <div className="blogList">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author} </p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
