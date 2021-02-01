fbbce778250aebd54d8ae53262346e46136fdcdc
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-01 12:39:15 +0330

    Learn how to use functions as props

--- react/tut/src/BlogList.js
+++ react/tut/src/BlogList.js
@@ -1,12 +1,17 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
+const BlogList = ({blogs,title,handleDelete}) => {
+
     return ( 
         <div className="blogList">
+            <h1> {title} </h1>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by {blog.author} </p>
+                    <button onClick={() => {
+                        handleDelete(blog.id)
+                    }}>Delete blog</button>
                 </div>
+
             ))}
         </div>
      );
