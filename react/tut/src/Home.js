3032853d780976194c6db5bb646305554541f432
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-01 13:14:29 +0330

    Learn how to use useEffect dependencies

--- react/tut/src/Home.js
+++ react/tut/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+
+    const [name,setName] = useState('mario')
     
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id)
@@ -15,12 +17,13 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('use effect ran')
-    })
+        console.log(name)
+    },[name])
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}>Click</button>
         </div>
      );
 }
