ef17f9061840c395658e0dd10046efa71ea49836
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-03 16:29:07 +0330

    Working With Components and Components Props

--- react/tr/src/App.js
+++ react/tr/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header' 
 
 function App() {
   const name = 'reza'
   return (
     <div className="container">
+      <Header name='reza'/>
       <h1>Hello from react</h1>
       <h2>Hello { name }</h2>
     </div>
