815f1700a7501181322ec8812a33cc22fe1ad3a4
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 15:59:52 +0330

    Working with dynamic routes in Next.js

---
+++ next.js/tut/pages/lists/[id].js
@@ -0,0 +1,41 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    const paths = data.map(list => {
+        return {
+            params: { id: list.id.toString() }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id 
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+    const data = await res.json()
+
+    return {
+        props: { list: data }
+    }
+}
+
+const Details = ({list}) => {
+    return ( 
+        <>
+            <h1> { list.name } </h1>
+
+            <p> { list.email } </p>
+
+            <p> { list.address.city } </p>
+            
+            <p> { list.website } </p>
+        </>
+     )
+}
+ 
+export default Details;
--- next.js/tut/pages/lists/index.js
+++ next.js/tut/pages/lists/index.js
@@ -1,4 +1,5 @@
 import styles from '../../styles/Lists.module.css'
+import Link from 'next/link'
 
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -14,11 +15,11 @@ const Lists = ({lists}) => {
         <div>
             <h1>All lists</h1>
             {lists.map(list => (
-                <div key={list.id} >
+                <Link href={'/lists/' + list.id}key={list.id} >
                     <a className={styles.single}>
                         <h3>{list.name}</h3>
                     </a>
-                </div>
+                </Link>
             ))}
         </div>
      );
