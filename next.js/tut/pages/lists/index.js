2835e5503d004f3bc59f1c0d0bded88f9a9db20c
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 15:33:27 +0330

    Learn how to fetch datas in Next.js

--- next.js/tut/pages/lists/index.js
+++ next.js/tut/pages/lists/index.js
@@ -1,7 +1,25 @@
-const Lists = () => {
+import styles from '../../styles/Lists.module.css'
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    return {
+        props: {lists: data}
+    }
+}
+
+const Lists = ({lists}) => {
     return ( 
         <div>
             <h1>All lists</h1>
+            {lists.map(list => (
+                <div key={list.id} >
+                    <a className={styles.single}>
+                        <h3>{list.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
