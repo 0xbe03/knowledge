c08127b4aa9bb972c2ceca855ce63e9a8ed7d247
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 14:44:46 +0330

    Working with Next.js Routes

---
+++ next.js/tut/pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ratione facere illum, esse iusto vel asperiores possimus, nulla beatae et dolor deleniti eos vitae sapiente, numquam iure? Nisi, eos cum?</p>
+        </div>
+     );
+}
+ 
+export default About;
