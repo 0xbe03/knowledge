2e6ab0c02fc9d08284109cd5f79ba104f3b29e3f
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 15:23:44 +0330

    Working with images in Next.js

--- next.js/tut/comps/Navbar.js
+++ next.js/tut/comps/Navbar.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
     return ( 
         <div>
             <nav>
                 <div className="logo">
-                    <h1>Reza Aghajani</h1>
+                    <Image src="/Personal.png" width={77} height={77} />
                 </div>
                 <Link href="/"><a>Home</a></Link>
                 <Link href="/about"><a>About</a></Link>
