5940379ffea1b994f2feb9414f4d8137e861722d
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 14:46:54 +0330

    Understand how pages and routes works in next.js

---
+++ next.js/tut/pages/lists/index.js
@@ -0,0 +1,9 @@
+const Lists = () => {
+    return ( 
+        <div>
+            <h1>All lists</h1>
+        </div>
+     );
+}
+ 
+export default Lists;
---
+++ next.js/tut/pages/lists/test.js
@@ -0,0 +1,11 @@
+const Test = () => {
+    return ( 
+        <div>
+            <h1>
+                This is a test
+            </h1>
+        </div>
+     );
+}
+ 
+export default Test;
