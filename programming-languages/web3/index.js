e7709f315d60dafac4b326c64e9909665bf1c18c
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-28 19:38:00 +0330

    Get account balance and convert it from wei to default eth

--- programming-languages/web3/index.js
+++ programming-languages/web3/index.js
@@ -1,8 +1,12 @@
 var Web3 = require('web3')
 
-var url = 'https://1p7ENMTratsqhWuxVfnR63RhMZa:97dd35787a8e7aa649b9f6927f257ce4@eth2-beacon-mainnet.infura.io'
+var url = 'HTTP://127.0.0.1:7545'
 
 var web3 = new Web3(url)
 
-var address = '0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8'
-console.log(web3.fromWei(web3.eth.getBalance(web3.eth.address))) 
+var address = '0x68231F0b756Fc9CC362dCCa34861716D9f90a91a'
+
+web3.eth.getBalance(address)
+    .then(res => balance = res)
+    .then(() => balance = web3.utils.fromWei(balance , 'ether'))
+    .then(() => console.log(balance))
